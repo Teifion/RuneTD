f17c3812b0743adb1a07686c7e62713727b8c7e8
Teifion <sarkalian@gmail.com>
2011-04-30 21:18:41 +0100

    Altered the way that the engine works (subclass rather than separate object).

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1 and sys.argv[1] == 'test':
 		raise Exception("Test mode not yet implimented")
 	else:
-		r = rune.Rune_game()
+		r = rune.RuneGame()
 		r.start()
