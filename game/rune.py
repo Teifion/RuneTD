c32b1d61d5f55154fa55e5a04dab4e60d0e65d94
Teifion <sarkalian@gmail.com>
2011-05-02 14:58:17 +0100

    Now places walls and walkways according to the levels.json file

--- game/rune.py
+++ game/rune.py
@@ -7,33 +7,26 @@ from game import classes
 
 class RuneGame (engine.EngineV2):
     name = "Rune TD"
+    tile_size = 35
     
     fps = 40
-    windowwidth = 1024
-    windowheight = 768
+    windowwidth = 40*tile_size
+    windowheight = 22*tile_size
     
     def __init__(self):
         super(RuneGame, self).__init__()
         
         self.resources = {
-            "bg_image": pygame.image.load('media/full_bg.png')
+            "bg_image": pygame.image.load('media/full_bg.png'),
+            "wall_image": pygame.image.load('media/wall.png'),
+            "walkway_image": pygame.image.load('media/walkway.png'),
         }
         
-        # Was used for testing some sprite stuff
-        # self.sprites.add(classes.Enemy((255, 255, 255), [10,10]))
-        # self.sprites.add(classes.Enemy((0, 255, 255), [10,10]))
-        # self.sprites.add(classes.Enemy((255, 0, 255), [10,10]))
-        # self.sprites.add(classes.Enemy((255, 255, 0), [10,10]))
-        # self.sprites.add(classes.Enemy((0, 0, 255), [10,10]))
-        # self.sprites.add(classes.Enemy((255, 0, 0), [10,10]))
-        # self.sprites.add(classes.Enemy((0, 255, 0), [10,10]))
-        # 
-        # e = classes.Enemy((255, 255, 255), [10,10])
-        # e.yeild = 0
-        # self.sprites.add(e)
+        self.walls = {}
+        self.walkways = {}
         
         # Testing the ability to load levels
-        self.load_level(5)
+        self.load_level(1)
     
     def startup(self):
         """docstring for startup"""
@@ -44,10 +37,15 @@ class RuneGame (engine.EngineV2):
         super(RuneGame, self).startup()
     
     def game_logic(self):
-        for s in self.sprites:
-            s.accelerate(self.mouse)
+        pass
+        # for s in self.sprites:
+        #     s.accelerate(self.mouse)
     
     def load_level(self, level):
+        # Wipeout all the existing level terrain
+        for k, s in self.walls.items(): s.kill()
+        for k, s in self.walkways.items(): s.kill()
+        
         level = str(level)
         
         with open('game/levels.json') as f:
@@ -62,4 +60,19 @@ class RuneGame (engine.EngineV2):
             except Exception as e:
                 raise
         
-        self.quit()
+        # Take them from data form and make them python objects
+        for y, row in enumerate(level_data['floor']):
+            for x, tile in enumerate(row):
+                pos = [x*self.tile_size, y*self.tile_size]
+                
+                if tile == "1":
+                    self.walls[(x, y)] = classes.Wall(self.resources['wall_image'], pos)
+                else:
+                    self.walkways[(x, y)] = classes.Walkway(self.resources['walkway_image'], pos)
+        
+        # Add them to our sprite group
+        for k, w in self.walls.items(): self.sprites.add(w)
+        for k, w in self.walkways.items(): self.sprites.add(w)
+        
+        
+
