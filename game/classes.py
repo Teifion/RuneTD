a73ea97a4363f6cd496232a42a0b1c12b61d95f2
Teifion <sarkalian@gmail.com>
2011-05-04 22:58:47 +0100

    Added very simple pathfinding behaviour

--- game/classes.py
+++ game/classes.py
@@ -7,41 +7,30 @@ class Enemy (pygame.sprite.Sprite):
         self.image = pygame.Surface([15, 15])
         self.image.fill(color)
         self.rect = self.image.get_rect()
-        self.rect.topleft = initial_position
+        self.rect.topleft = (-100, -100)# Start offscreen
+        self.rect.topleft = (10,10)
         
         self.next_update_time = 0 # update() hasn't been called yet.
         
-        self.velocity = [0, 0]
+        self.position = list(initial_position)
+        self.target = (0, 0)
         
-        self.yeild = random.random()/3
-        self.acceleration = 1
-
+        self.move_speed = 1
+        
     def update(self, current_time):
         # Update every 10 milliseconds = 1/100th of a second.
         if self.next_update_time < current_time:
-            self.rect.left += self.velocity[0]
-            self.rect.top += self.velocity[1]
+            if self.position[0] < self.target[0]:
+                self.position[0] = min(self.position[0] + self.move_speed, self.target[0])
+            elif self.position[0] > self.target[0]:
+                self.position[0] = max(self.position[0] - self.move_speed, self.target[0])
+            
+            if self.position[1] < self.target[1]:
+                self.position[1] = min(self.position[1] + self.move_speed, self.target[1])
+            elif self.position[1] > self.target[1]:
+                self.position[1] = max(self.position[1] - self.move_speed, self.target[1])
+            
+            self.rect.left = self.rect.left + 1#self.position[0] * 35 + 17
+            self.rect.top = 100#self.position[1] * 35 + 17
             
             self.next_update_time = current_time + 10
-    
-    def accelerate(self, target):
-        x, y = self.rect.topleft
-        tx, ty = target
-        
-        # X
-        if random.random() > self.yeild:
-            if x < tx:
-                self.velocity[0] += self.acceleration
-            else:
-                self.velocity[0] -= self.acceleration
-        
-        # Y
-        if random.random() > self.yeild:
-            if y < ty:
-                self.velocity[1] += self.acceleration
-            else:
-                self.velocity[1] -= self.acceleration
-        
-        
-        self.velocity[0] = min(max(self.velocity[0], -8), 8)
-        self.velocity[1] = min(max(self.velocity[1], -8), 8)
