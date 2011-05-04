e715453b326d54ad52c5114c9c008c2dee05bf6e
Teifion <sarkalian@gmail.com>
2011-05-04 22:58:59 +0100

    Added pathfinding function to find a way through the maze

--- game/rune.py
+++ game/rune.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 import pygame
 
@@ -26,9 +27,8 @@ class RuneGame (engine.EngineV2):
             "end_image": pygame.image.load('media/end.png'),
         }
         
-        self.sprites.add(classes.Enemy((255,0,0), (50,50)))
-        
         self.tiles = {}
+        self.pathway = {}
         
         self.start_tile = (1,0)
         self.end_tile = (0,0)
@@ -42,6 +42,8 @@ class RuneGame (engine.EngineV2):
         super(RuneGame, self).startup()
         
         self.load_level(1)
+        
+        self.sprites.add(classes.Enemy((255,0,0), self.start_tile))
     
     def game_logic(self):
         pass
@@ -84,18 +86,105 @@ class RuneGame (engine.EngineV2):
                     self.background.blit(self.resources['wall_image'], pos)
                 elif tile == "S":
                     self.background.blit(self.resources['start_image'], pos)
-                    self.end_tile = pos
+                    self.start_tile = x,y
                 elif tile == "E":
                     self.background.blit(self.resources['end_image'], pos)
-                    self.start_tile = pos
+                    self.end_tile = x,y
                 else:
                     raise KeyError("Key of '{0}' could not be handled".format(tile))
-        #             
-        # 
-        # # Add them to our sprite group
-        # for k, w in self.walls.items(): self.sprites.add(w)
-        # for k, w in self.walkways.items(): self.sprites.add(w)
-        # 
-        # if self.start_tile != None: self.sprites.add(self.start_tile)
-        # if self.end_tile != None: self.sprites.add(self.end_tile)
+        
+        
+        # Now to pathfind
+        self.build_pathway()
+    
+    def build_pathway(self):
+        """
+        Builds a pathway through the maze so that enemies know where to go.
+        """
+        def distance(pos1, pos2):
+        	x = abs(pos1[0] - pos2[0])
+        	y = abs(pos1[1] - pos2[1])
+        	return math.sqrt(x*x + y*y)
+        
+        walked_tiles = set()
+        dead_tiles = set()
+        steps = []
+        
+        success = False
+        
+        # We start with the start_tile
+        steps.append((self.start_tile, 9999))
+        
+        while not success:
+            # Get our current position
+            try:
+                x, y = steps[-1][0]
+            except Exception as e:
+                # No steps? Means we've not got anywhere to go
+                if steps == []:
+                    raise Exception("No pathway found")
+                raise
+            
+            view_dict = {
+                "n":    (x, y-1),
+                "e":    (x+1, y),
+                "s":    (x, y+1),
+                "w":    (x-1, y),
+            }
 
+            # Set the best_cost to something that any tile beats
+            best_tile = [
+                (-1,-1),# Tile
+                9999999,# Distance
+            ]
+            
+            for k, v in view_dict.items():
+                if v == self.end_tile:
+                    best_tile = [v, -1]
+                    success = True
+                    continue
+                
+                # We don't want to move back and forth all the time
+                if v in walked_tiles: continue
+                if v in dead_tiles: continue
+                
+                if v in self.tiles:
+                    tile_type = self.tiles[v]
+                else:
+                    dead_tiles.add(v)
+                    continue
+                
+                # If it's not a walkway we can't use it
+                if tile_type != " ":
+                    dead_tiles.add(v)
+                    continue
+                
+                new_remaining = distance(v, self.end_tile)
+                
+                # Rank it
+                if new_remaining < best_tile[1]:
+                    best_tile = [v, new_remaining]
+            
+            # Dead end?
+            if best_tile[0] == (-1,-1):
+                # Lets jump back 1 step
+                del(steps[-1])
+                
+            else:
+                # We should now have a tile to move to
+                s = tuple(best_tile)
+                steps.append(s)
+            
+            # Ensure we don't go back over this one
+            walked_tiles.add(best_tile[0])
+        
+        # We now have a list of steps taken
+        self.pathway = {}
+        last_step = None
+        for tile, dist in steps:
+            self.pathway[tile] = {"previous":last_step, "next":None}
+            
+            if last_step != None:
+                self.pathway[last_step]["next"] = tile
+            last_step = tile
+    
