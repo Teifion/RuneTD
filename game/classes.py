25c9554f5e8eaa11dc0e2ab2c1a5c637ed67117d
Teifion <sarkalian@gmail.com>
2011-05-07 20:45:42 +0100

    Added rune class and tweaked the enemies class to take some information from it's game

--- game/classes.py
+++ game/classes.py
@@ -2,18 +2,19 @@ import pygame
 import random
 
 class Enemy (pygame.sprite.Sprite):
-    def __init__(self, color, initial_position):
+    def __init__(self, game, color, position):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([15, 15])
+        self.image = pygame.Surface([game.enemy_size, game.enemy_size])
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.topleft = (-100, -100)# Start offscreen
-        self.rect.topleft = (10,10)
         
         self.next_update_time = 0 # update() hasn't been called yet.
         
-        self.position = list(initial_position)
-        self.target = tuple(initial_position)
+        self.position = list(position)
+        self.target = tuple(position)
+        
+        self.offset = game.tile_size/2 - game.enemy_size/2
         
         self.move_speed = 1
         
@@ -30,7 +31,37 @@ class Enemy (pygame.sprite.Sprite):
             elif self.position[1] > self.target[1]:
                 self.position[1] = max(self.position[1] - self.move_speed, self.target[1])
             
-            self.rect.left = self.position[0] * 35 + 18 - self.rect.width/2
-            self.rect.top = self.position[1] * 35 + 18 - self.rect.height/2
+            self.rect.left = self.position[0] * 35 + self.offset
+            self.rect.top = self.position[1] * 35 + self.offset
             
             self.next_update_time = current_time + 10
+
+class Rune (pygame.sprite.Sprite):
+    def __init__(self, game, position):
+        pygame.sprite.Sprite.__init__(self)
+        # Image is set by subclass
+        self.image = pygame.Surface([game.rune_size, game.rune_size])
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (-100, -100)# Start offscreen
+        
+        self.next_update_time = 0 # update() hasn't been called yet.
+        
+        self.position = list(position)
+        
+        self.offset = game.tile_size/2 - game.rune_size/2
+        
+        self.move_speed = 1
+    
+    def update(self, current_time):
+        if self.next_update_time < current_time or True:
+            self.rect.left = self.position[0] * 35 + self.offset
+            self.rect.top = self.position[1] * 35 + self.offset
+            
+            self.next_update_time = current_time + 10
+
+class Pink_rune (Rune):
+    def __init__(self, game, position):
+        super(Pink_rune, self).__init__(game, position)
+        self.image = game.resources['pink_rune']
+        
+        
