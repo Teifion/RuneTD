8d1d69b1d626c4be09374ba73bcd6265499e2359
Teifion <sarkalian@gmail.com>
2011-05-09 00:12:46 +0100

    Added in shots

--- game/classes.py
+++ game/classes.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+import math
+
 class Enemy (pygame.sprite.Sprite):
     def __init__(self, game, color, position):
         pygame.sprite.Sprite.__init__(self)
@@ -17,7 +19,8 @@ class Enemy (pygame.sprite.Sprite):
         self.offset = game.tile_size/2 - game.enemy_size/2
         
         self.move_speed = 1
-        
+        self.chasers = []
+    
     def update(self, current_time):
         # Update every 10 milliseconds = 1/100th of a second.
         if self.next_update_time < current_time or True:
@@ -45,23 +48,120 @@ class Rune (pygame.sprite.Sprite):
         self.rect.topleft = (-100, -100)# Start offscreen
         
         self.next_update_time = 0 # update() hasn't been called yet.
+        self.last_shot = 0
+        self.fire_speed = 500
         
         self.position = list(position)
         
         self.offset = game.tile_size/2 - game.rune_size/2
         
-        self.move_speed = 1
+        self.target = None
+        self.range = 6
+        self.game = game
     
     def update(self, current_time):
         if self.next_update_time < current_time or True:
             self.rect.left = self.position[0] * 35 + self.offset
             self.rect.top = self.position[1] * 35 + self.offset
             
+            if current_time > self.last_shot + self.fire_speed:
+                self.shoot()
+                self.last_shot = current_time
+            
             self.next_update_time = current_time + 10
+        
+    def shoot(self):
+        # Lose target if it goes out of range
+        if self.target != None and self.distance(self.target) > self.range:
+            self.target = None
+        
+        # Pick a target from the enemies in the list
+        if self.target == None:
+            for e in self.game.enemies:
+                if self.distance(e) <= self.range:
+                    self.target = e
+                    break
+        
+        if self.target != None:
+            s = self.shot_type(self.game, self.position, self.target)
+            self.game.add_shot(s)
+        else:
+            print("No target")
+    
+    def distance(self, enemy):
+        x = abs(self.position[0] - enemy.position[0])
+        y = abs(self.position[1] - enemy.position[1])
+        return math.sqrt(x*x + y*y)
 
 class Pink_rune (Rune):
     def __init__(self, game, position):
         super(Pink_rune, self).__init__(game, position)
         self.image = game.resources['pink_rune']
+        self.shot_type = Pink_bullet
+
+
+class Bullet (pygame.sprite.Sprite):
+    def __init__(self, game, position, target):
+        pygame.sprite.Sprite.__init__(self)
+        # Image is set by subclass
+        self.image = pygame.Surface([8, 8])
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (-100, -100)# Start offscreen
         
+        self.next_update_time = 0 # update() hasn't been called yet.
         
+        self.position = list(position)
+        
+        self.offset = game.tile_size/2 - self.rect.width/2
+        
+        self.move_speed = 0.5
+        
+        self.game = game
+        self.seeking = False
+        
+        if type(target) == list or type(target) == tuple:
+            self.sprite_target = None
+            self.target = target
+        else:
+            target.chasers.append(self)
+            self.sprite_target = target
+            self.target = target.position
+    
+    def update(self, current_time):
+        if self.next_update_time < current_time or True:
+            if self.sprite_target != None:
+                self.target = self.sprite_target.position
+            
+            if self.position[0] < self.target[0]:
+                self.position[0] = min(self.position[0] + self.move_speed, self.target[0])
+            elif self.position[0] > self.target[0]:
+                self.position[0] = max(self.position[0] - self.move_speed, self.target[0])
+            
+            if self.position[1] < self.target[1]:
+                self.position[1] = min(self.position[1] + self.move_speed, self.target[1])
+            elif self.position[1] > self.target[1]:
+                self.position[1] = max(self.position[1] - self.move_speed, self.target[1])
+            
+            if self.distance() < 0.2:
+                self.hit()
+            
+            self.rect.left = self.position[0] * 35 + self.offset
+            self.rect.top = self.position[1] * 35 + self.offset
+            
+            self.next_update_time = current_time + 10
+    
+    def distance(self):
+        x = abs(self.position[0] - self.target[0])
+        y = abs(self.position[1] - self.target[1])
+        return math.sqrt(x*x + y*y)    
+    
+    def hit(self):
+        if self.sprite_target != None:
+            self.game.remove_enemy(self.sprite_target)
+        
+        self.game.remove_shot(self)
+
+class Pink_bullet (Bullet):
+    def __init__(self, game, position, target):
+        super(Pink_bullet, self).__init__(game, position, target)
+        self.image = game.resources['pink_bullet']
