753857058f801dc7cf86c60a3ebcbf350a8476e0
Teifion <sarkalian@gmail.com>
2011-05-14 21:27:22 +0100

    Changed name of rune.py to rune_game.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from game import rune
+from game import rune_game
 import sys
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1 and sys.argv[1] == 'test':
 		raise Exception("Test mode not yet implimented")
 	else:
-		r = rune.RuneGame()
+		r = rune_game.RuneGame()
 		r.start()
